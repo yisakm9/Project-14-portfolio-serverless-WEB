b9a71599905500620aadaece950c3a5ac7cb879c
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-12-21 17:20:07 +0300

    feat: append random suffix to frontend bucket name for uniqueness

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -6,7 +6,7 @@ module "frontend_bucket" {
   source = "../../modules/s3_website"
 
   # We use a variable here to keep the environment flexible
-  bucket_name = "${var.project_name}-frontend-${var.environment}"
+  bucket_name = "${var.project_name}-frontend-${var.environment}-${random_pet.suffix.id}"
   
   tags = {
     Environment = var.environment
